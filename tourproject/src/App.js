8e00958dd4d41da5f8adceb966cc21284fcc11fc
OzSlapak <ozgeslapak@gmail.com>
2022-10-11 13:47:54 -0400

    first step

---
+++ tourproject/src/App.js
@@ -0,0 +1,14 @@
+import './App.scss';
+import Navbar from './components/Navbar/Navbar';
+
+const App = () => {
+  return(
+    <div className ="App">
+      <Navbar/>
+      <h1>Welcome to react</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis iure, mollitia, voluptates libero eos quidem animi odio cum illum culpa nesciunt asperiores labore. Ducimus mollitia natus exercitationem rem, ex asperiores!</p>
+    </div>
+  )
+}
+
+export default App;
---
+++ tourproject/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import './Navbar.scss';
+
+
+const Navbar = ()=>{
+    return(
+        <div className ="navbar">
+            <a href="#aboutus">About Us</a>
+            <a href="#foryou">About Us</a>
+            <a href="#services">About Us</a>
+            <a href="#blog">About Us</a> 
+            <a href="vlog">About Us</a>
+            <a href="content">About Us</a>
+        </div>
+    )
+}
+
+export default Navbar;

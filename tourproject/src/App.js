e4daf7aeb86425296cf3f2ea5a6abb77c233e221
OzSlapak <ozgeslapak@gmail.com>
2022-10-11 13:54:34 -0400

    headerdone

--- tourproject/src/App.js
+++ tourproject/src/App.js
@@ -1,10 +1,12 @@
 import './App.scss';
 import Navbar from './components/Navbar/Navbar';
+import Header from './components/Header/Header';
 
 const App = () => {
   return(
     <div className ="App">
       <Navbar/>
+      <Header/>
       <h1>Welcome to react</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis iure, mollitia, voluptates libero eos quidem animi odio cum illum culpa nesciunt asperiores labore. Ducimus mollitia natus exercitationem rem, ex asperiores!</p>
     </div>
